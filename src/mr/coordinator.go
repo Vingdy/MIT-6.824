b2e7ac81e36ed0a2abd17d1241b08663402f824c
Ving <705105278@qq.com>
2021-07-22 11:22:23 +0800

    定义协调器数据结构

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,6 +1,10 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"math"
+	"sync"
+)
 import "net"
 import "os"
 import "net/rpc"
@@ -9,7 +13,18 @@ import "net/http"
 
 type Coordinator struct {
 	// Your definitions here.
+	lock sync.Mutex//并发， 锁定共享数据
+	status int//0:finish, 1:map, 2:reduce
+	nMap int//分配map数量
+	nReduce int//分配reduce数量
+	task map[int]Task//分配任务记录
+	allocTask chan Task//待分配任务管道
+}
 
+type Task struct {
+	Type int
+	Index int
+	File string
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -60,10 +75,24 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		status: 1,
+		nMap: len(files),
+		nReduce: nReduce,
+		task: make(map[int]Task),
+		allocTask: make(chan Task, int(math.Max(float64(len(files)), float64(nReduce)))),
+	}
 
 	// Your code here.
-
+	for i, file := range files {
+		task := Task{
+			Type: 1,
+			Index: i,
+			File: file,
+		}
+		c.task[task.Type] = task
+		c.allocTask <- task
+	}
 
 	c.server()
 	return &c
